87b9a8889407eff236364dda06a0f28c1db63416
hanfrend <sunhyung2007@naver.com>
2020-12-04 20:55:19 +0900

    view 수정

--- pybo/views.py
+++ pybo/views.py
@@ -2,4 +2,4 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("안녕하세요 오준호입니다. + 안녕하세요 정채은입니다. ")
+    return HttpResponse("안녕하세요 오준호입니다. + 안녕하세요 정채은입니다. + 안녕하세요 김선형입니다.")
