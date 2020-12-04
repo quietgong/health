1603eb5939454f5effd0ab19ae8b259178b6b486
LAPTOP-GMU6JBJ8\quiet <quietgong@naver.com>
2020-12-05 01:07:55 +0900

    디자인 틀 만드는중

--- config/urls.py
+++ config/urls.py
@@ -19,6 +19,7 @@ from django.urls import path
 from pybo import views
 
 urlpatterns = [
+    path('', views.index),
     path('admin/', admin.site.urls),
     path('pybo/', views.index),
 ]
--- pybo/views.py
+++ pybo/views.py
@@ -1,5 +1,5 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def index(request):
-    return HttpResponse("우리는 멋쟁이들")
+    return render(request, 'index.html')
